44032d85f59575e8b5a665e61eb72973349b23ed
vardhan12178 <balavardhan12178@gmail.com>
2024-06-08 16:33:34 +0530

    made changes to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ import DarkModeToggle from '../DarkModeToggle';
 function Header() {
     return (
         <header className="bg-gray-300 dark:bg-gray-800 p-4 shadow-md">
-            <nav className="container mx-auto flex justify-centeritems-center">
+            <nav className="container mx-auto flex justify-between items-center">
                 <h1 className="text-2xl md:text-3xl font-bold text-teal-600 dark:text-teal-300 text-center md:text-left w-full md:w-auto">WikiSearch</h1>
                 <div className="flex justify-end w-full md:w-auto">
                     <DarkModeToggle />
